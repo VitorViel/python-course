6448362d6f6141e2740d6ec8799e22f18f5da826
Vitor Viel <vitorandreviel@gmail.com>
2022-02-03 15:04:47 -0300

    second course work

--- python-course2/modulo3/exercicios80.py
+++ python-course2/modulo3/exercicios80.py
@@ -53,16 +53,87 @@ lista[1].sort()
 print(f'A lista de ímpares ficou {lista[1]}')
 '''
 
-'''Exercicio 86 matrizes 
+'''Exercicio 86 e 87 matrizes
+soma_pares = 0
+soma_terceira = 0
+maior_segunda = 0
 matriz = [[0,0,0], [0,0,0], [0,0,0]]
 for l in range(0,3):
   for c in range(0,3):
     matriz[l][c] = int(input('Digite o valor pra ser adicionado a matriz\n-> '))
+    if l == 1:
+      if matriz[l][c] > maior_segunda:
+        maior_segunda = matriz[l][c]
+    if matriz[l][c] % 2 == 0:
+      soma_pares += matriz[l][c]
+    if c == 2:
+      soma_terceira += matriz[l][c]
 print('-=' * 30)
 for l in range(0,3):
   for c in range(0,3):
     print(f'[{matriz[l][c]:^6}]', end=' ')
   print()
 print('-=' * 30)
+print(f'A soma dos valores pares é: {soma_pares}')
+print(f'A soma dos valores ta terceira coluna é {soma_terceira}')
+print(f'O maior valor da segunda linha é {maior_segunda}')
 '''
+
+from random import randint
+'''Exercicio 88 ajude na mega sena
+from random import randint
+from time import sleep
+print('-' * 40)
+print(f'{"JOGA NA MEGA SENA":^40}')
+print('-' * 40)
+quantidade_jogos = int(input('Quantos jogos você quer que eu sorteie? '))
+print(f'=-=-=-= SORTEANDO {quantidade_jogos} JOGOS =-=-=-=')
+sleep(1)
+for c in range(0, quantidade_jogos):
+    jogo = []
+    while len(jogo) != 6:
+        num = randint(1, 60)
+        if num not in jogo:
+            jogo.append(num)
+    print(f'Jogo {c + 1}: {sorted(jogo)}')
+    sleep(1)
+print('=-=-=-=-= < BOA SORTE! > =-=-=-=-=')
+'''
+
+'''Exercicio 89 sistema completo de boletim'''
+
+ficha = []
+
+while True:
+  nome = str(input('Nome\n-> '))
+  nota1 = float(input('nota1\n-> '))
+  nota2 = float(input('nota2\n-> '))
+  media = (nota1 + nota2) / 2
+  ficha.append([nome,[nota1,nota2],media])
+  continuar = str(input('continuar? [S/N]'))
+  if continuar in 'Nn':
+    break
+
+print('*'*15, end='')
+print('BOLETIM', end='')
+print('*'*15,)
+print('N° ///// NOME //// MÉDIA')
+print('-------------------------')
+for i, a in enumerate(ficha):
+  print(f'{i} ///// {a[0]} ///// {a[2]}')
+print('*'*35)
+
+while True:
+  aluno = int(input('Digite o número do aluno que deseja ver as notas [999 encerra]\n-> '))
+  if aluno == 999:
+    print('Foi um prazer')
+    break
+  if aluno <= len(ficha) - 1:
+    print(f'As notas do aluno {aluno}° == {ficha[aluno][0]} e {ficha[aluno][1]}')
+print('GG')
+  
+  
+  
+
+
 
